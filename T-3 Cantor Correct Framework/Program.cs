f6374013d2533b49bb177fa160d0ce06f57884c8
bcantor2000 <bcantor@usf.edu>
2022-02-22 14:42:20 -0500

    Add project files.

---
+++ T-3 Cantor Correct Framework/Program.cs
@@ -0,0 +1,40 @@
+﻿//Brian Cantor
+//ISM 4300
+//Assignment: T-3 Using Iterative Statments
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_3_Cantor_Correct_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Retrieve input value from user and convert to integer
+            Console.WriteLine("Enter a value between 5 and 200 to see conversions");
+            string input = Console.ReadLine();
+            int inputValue = int.Parse(input);
+            //if loop used to determine if value is between 5 and 25, as stated in assignment instructions
+            if ((inputValue > 5) && (inputValue < 25))
+            {
+                //start every table with o for both CAD and USD
+                Console.WriteLine("CAD = 0" + " " + "USD = 0");
+                //while loop used to create a table displaying values all the way up to 200 and to never exceed 200
+                while (inputValue <= 200)
+                {
+                    double usValue = inputValue * 0.792367;
+                    Console.WriteLine("CAD =" + inputValue.ToString() + "USD =" + usValue);
+                    inputValue += 10;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
